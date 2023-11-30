023e9040491265437ff344c2a6ee6c5aa2ae21fb
Pranshu Srivastava <iampranshu24@gmail.com>
2023-11-30 10:37:34 +0000

    fix: updated init.sql
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- django-postgres/django_postgres/sql/init.sql
+++ django-postgres/django_postgres/sql/init.sql
@@ -1,2 +1,10 @@
-psql -U postgres
-postgres=# CREATE DATABASE usersdb;
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT FROM pg_database WHERE datname = 'usersdb'
+    )
+    THEN
+        CREATE DATABASE usersdb;
+    END IF;
+END
+$$;
