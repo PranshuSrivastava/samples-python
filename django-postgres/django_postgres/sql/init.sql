693d71c5706b334662c7541fd52324e598863b2a
Pranshu Srivastava <iampranshu24@gmail.com>
2023-12-07 18:04:39 +0000

    fix: updated samples python
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- django-postgres/django_postgres/sql/init.sql
+++ django-postgres/django_postgres/sql/init.sql
@@ -1,2 +1,26 @@
-SELECT 'CREATE DATABASE usersdb'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'usersdb')\gexec
+-- Check if the database exists and create it if it doesn't
+DO $$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'usersdb') THEN
+      CREATE DATABASE usersdb;
+   END IF;
+END
+$$;
+
+-- Connect to the new database and create the table if it doesn't exist
+\c usersdb
+
+DO $$
+BEGIN
+   -- Check if the table exists, create it if it doesn't
+   IF NOT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename  = 'application_user') THEN
+      CREATE TABLE public.application_user (
+         id UUID PRIMARY KEY,
+         name VARCHAR(100),
+         email VARCHAR(100),
+         password VARCHAR(100),
+         website VARCHAR(100)
+      );
+   END IF;
+END
+$$;
