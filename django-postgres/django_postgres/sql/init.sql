638681a495e998670e0e98cef1ac561002754b6f
Pranshu Srivastava <iampranshu24@gmail.com>
2023-12-08 08:32:45 +0000

    fix: update init.sql
    Signed-off-by: Pranshu Srivastava <iampranshu24@gmail.com>

--- django-postgres/django_postgres/sql/init.sql
+++ django-postgres/django_postgres/sql/init.sql
@@ -1,26 +1,19 @@
--- Check if the database exists and create it if it doesn't
-DO $$
-BEGIN
-   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'usersdb') THEN
-      CREATE DATABASE usersdb;
-   END IF;
-END
-$$;
+-- Attempt to connect to 'usersdb', if it doesn't exist, connect to the default 'postgres' database
+\c usersdb ON_ERROR_STOP on
+\c postgres
 
--- Connect to the new database and create the table if it doesn't exist
+-- Check if the database exists and create it if it doesn't
+SELECT 'CREATE DATABASE usersdb'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'usersdb')\gexec
+
+-- You need to reconnect to the new database to create the table
 \c usersdb
 
-DO $$
-BEGIN
-   -- Check if the table exists, create it if it doesn't
-   IF NOT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename  = 'application_user') THEN
-      CREATE TABLE public.application_user (
-         id UUID PRIMARY KEY,
-         name VARCHAR(100),
-         email VARCHAR(100),
-         password VARCHAR(100),
-         website VARCHAR(100)
-      );
-   END IF;
-END
-$$;
+-- Create the table if it doesn't exist
+CREATE TABLE IF NOT EXISTS application_user (
+   id UUID PRIMARY KEY,
+   name VARCHAR(100),
+   email VARCHAR(100),
+   password VARCHAR(100),
+   website VARCHAR(100)
+);
